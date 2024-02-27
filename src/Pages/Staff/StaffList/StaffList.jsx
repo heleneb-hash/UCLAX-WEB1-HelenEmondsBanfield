ca63fac7a11639757b32a131dad5301b57b071fc
Helen Emonds-banfield <hebanfield@gmail.com>
2024-02-26 18:46:58 -0800

    L6: Complete

---
+++ src/Pages/Staff/StaffList/StaffList.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { staffData } from "../staffData.jsx";
+
+import StaffMember from "./StaffMember.jsx";
+
+const StaffList = () => {
+    console.log({ staffData });
+    return (
+        <StaffListStyled>
+            {staffData.map((member) => {
+                return <StaffMember key={member.id} member={member} />;
+            })}
+        </StaffListStyled>
+    );
+};
+
+export default StaffList;
+
+const StaffListStyled = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    gap: 1rem;
+`;
